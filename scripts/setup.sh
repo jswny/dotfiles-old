1db5004f8349695c7d701f84019e8d1145a1217b
Joe Sweeney <joe@j1.io>
2020-02-19 01:40:02 -0500

    Fix setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,11 @@
 # This script only covers installation of the dotfiles themselves, and does not handle installing any dependencies
 # All dependencies should be installed before running this script
 
+script_name=$(basename "$0")
+log() {
+  echo "[$script_name] $1"
+}
+
 # Set the XDG configuration directory if it is not already set
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"/root/.config"}
 
@@ -12,7 +17,7 @@ ln -s "$PWD/fish/config.fish" "$XDG_CONFIG_HOME/fish/config.fish"
 ln -s "$PWD/fish/fishfile" "$XDG_CONFIG_HOME/fish/fishfile"
 
 # Install plugins with Fisher
-echo "Installing Fisher plugins..."
+log "Installing Fisher plugins..."
 fish -c "fisher"
 
 # Symlink Tmux files
@@ -26,8 +31,8 @@ ln -s "$PWD/tmux/.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 ln -s "$XDG_CONFIG_HOME/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 # Install TPM plugins
-echo "Installing TPM plugins..."
-$XDG_DATA_HOME/tmux/plugins/tpm/bin/install_plugins
+log "Installing TPM plugins..."
+"$XDG_DATA_HOME"/tmux/plugins/tpm/bin/install_plugins
 
 # Symlink NeoVim files
 mkdir -p "$XDG_CONFIG_HOME/nvim"
@@ -35,5 +40,5 @@ ln -s "$PWD/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 ln -s "$PWD/nvim/lcnv-settings.json" "$XDG_CONFIG_HOME/nvim/lcnv-settings.json"
 
 # Install NeoVim plugins and output to log file since this output is not noninteractive
-echo "Installing NeoVim plugins..."
+log "Installing NeoVim plugins..."
 nvim --headless '+PlugInstall --sync' +qa &> /var/log/nvim_plug_install.log
